89c3cc2a1fd651bf46d3f8f52ba75f73febbfdd7
Rocket2510 <rocket251019@gmail.com>
2023-03-26 17:55:59 -0600

    Nueva practica

---
+++ Actividad 5/Practica 2/Matematicas/src/Interfaz/Matematicas.java
@@ -0,0 +1,10 @@
+package Interfaz;
+
+public interface Matematicas {
+    public static final double PI=3.1415926535;
+    public double radianes(double angulo);
+    public double grados(double angulo);
+    public double seno(double ang);
+    public double coseno(double ang);
+
+}
---
+++ Actividad 5/Practica 2/Matematicas/src/main/Trigonometria.java
@@ -0,0 +1,32 @@
+package main;
+import Interfaz.Matematicas;
+
+public class Trigonometria implements Matematicas{
+
+    
+
+    @Override
+    public double radianes(double angulo) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'radianes'");
+    }
+
+    @Override
+    public double grados(double angulo) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'grados'");
+    }
+
+    @Override
+    public double seno(double ang) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'seno'");
+    }
+
+    @Override
+    public double coseno(double ang) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'coseno'");
+    }
+    
+}
